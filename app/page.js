05fa28cd98530446d7c81006d72e24069ab68fda
smyxxz <105841105122@student.unismuh.ac.id>
2024-08-17 18:15:39 +0700

    selesai chapter 3

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-
+import Image from "next/image";
 export default function Page() {
   return (
     <main className="flex flex-col min-h-screen p-6">
       <div className="flex items-end h-20 p-4 bg-blue-500 rounded-lg shrink-0 md:h-52">
-        {/* <AcmeLogo /> */}
+      <AcmeLogo />
       </div>
       <div className="flex flex-col gap-4 mt-4 grow md:flex-row">
         <div className="flex flex-col justify-center gap-6 px-6 py-10 rounded-lg bg-gray-50 md:w-2/5 md:px-20">
@@ -26,6 +26,20 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+        src="/hero-desktop.png"
+        width={1000}
+        height={760}
+        className="hidden md:block"
+        alt="Screenshots of the dashboard project showing desktop version"
+      />
+      <Image
+        src="/hero-mobile.png"
+        width={560}
+        height={620}
+        className=" block md:hidden"
+        alt="Screenshots of the dashboard project showing desktop version"
+      />
         </div>
       </div>
     </main>
