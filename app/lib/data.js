405c45181db3acc54e7d8578ee4092d79455e391
smyxxz <105841105122@student.unismuh.ac.id>
2024-08-28 18:16:52 +0700

    selesai chapter 11

--- app/lib/data.js
+++ app/lib/data.js
@@ -76,6 +76,8 @@ export async function fetchCardData() {
 }
 
 const ITEMS_PER_PAGE = 6;
+
+
 export async function fetchFilteredInvoices(query, currentPage) {
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
